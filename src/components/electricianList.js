52b966a867e00976bdb8979a9d06761edfc44185
Rajeswaran <rajeswaran149@gmail.com>
2024-09-10 11:51:57 +0530

    update  slidebar style

--- src/components/electricianList.js
+++ src/components/electricianList.js
@@ -138,7 +138,7 @@ const ElectricianList = () => {
         <table className="min-w-full divide-y divide-gray-200">
           <thead className="bg-gray-50">
             <tr>
-              <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-solid border-2">ID</th>
+              <th className="px-6 py-3 whitespace-nowrap text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-solid border-2">ID</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-solid border-2">Name</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-solid border-2">Contact</th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider border-solid border-2">Occupation</th>
--- src/components/slidebar.js
+++ src/components/slidebar.js
@@ -42,7 +42,7 @@ const Sliderbar = () => {
     return (
         <nav className={`h-full border-[2px] border-solid border-transparent rounded-md ease-in-out duration-700 ${isOpen ? "sm:w-72 w-[235px] absolute md:static bg-white md:bg-transparent" : "w-12"} overflow-hidden mr-3`}>
             <div className="flex items-center h-16 w-full border-b-2 border-solid border-[#D3D3D3]">
-                <h2 className={`ease-in-out duration-700 text-amber-950 ${isOpen ? "w-52 h-full" : "w-0 h-full"}`} > 𝙀𝙈𝙎 </h2>
+                <h2 className={`flex  items-center ease-in-out duration-700 text-amber-950 ${isOpen ? "w-52 h-full" : "w-0 h-full"}`} > 𝙀𝙈𝙎 </h2>
                 <div
                     className={`h-9 w-9 mx-auto text-3xl flex justify-center items-center rounded-md cursor-pointer text-[#135049] ${isOpen ? "bg-amber-950 text-white" : "hover:bg-amber-950 hover:text-white transition-all duration-500"}`}
                     onClick={toggle}
@@ -56,7 +56,7 @@ const Sliderbar = () => {
                         to={item.path}
                         key={index}
                         className={({ isActive }) =>
-                            `flex items-center whitespace-nowrap py-3 px-3 gap-4 mb-3 no-underline transition-all duration-500 ${isActive ? "bg-amber-950 text-white rounded-md" : "text-[#135049]"} hover:bg-amber-950 hover:text-white hover:rounded-md`
+                            `flex items-center whitespace-nowrap py-2 px-2 gap-4 mb-3 no-underline transition-all duration-500 ${isActive ? "bg-amber-950 text-white rounded-md" : "text-[#135049]"} hover:bg-amber-950 hover:text-white hover:rounded-md`
                         }
                     >
                         <div className="text-[21px]">{item.icon}</div>
