be2a08cd85df1485b117e401dbbb2234d50db8f3
Rajeswaran <rajeswaran149@gmail.com>
2024-09-14 13:27:46 +0530

    update base url

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,12 +1,13 @@
 import axios from 'axios';
+import { base_url } from '../config';
 
    export const fetchElectricians = () => async dispatch => {
-     const response = await axios.get('http://localhost:5000/api/electricians');
+     const response = await axios.get(`${base_url}/api/electricians`);
      console.log(response)
      dispatch({ type: 'FETCH_ELECTRICIANS', payload: response.data });
    };
 
    export const fetchComplaints = () => async dispatch => {
-     const response = await axios.get('http://localhost:5000/api/complaints');
+     const response = await axios.get(`${base_url}/api/complaints`);
      dispatch({ type: 'FETCH_COMPLAINTS', payload: response.data });
    };
